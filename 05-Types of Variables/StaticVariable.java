8dd053cb6bce41b6c69279d8f9acd4859a81f6e3
jp-devhub <jpdevhub@gmail.com>
2026-02-07 14:35:55 +0530

    Create StaticVariable.java

---
+++ 05-Types of Variables/StaticVariable.java
@@ -0,0 +1,14 @@
+public class StaticVariable{
+
+    static int age = 18;
+
+
+   public static void main(String[] args){
+
+       System.out.println(age);
+
+       System.out.println(StaticVariable.age);
+
+   }
+
+}
