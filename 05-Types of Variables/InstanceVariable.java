8548157f11851de7e3f5207b220795c3572dfe1c
jp-devhub <jpdevhub@gmail.com>
2026-02-07 14:35:39 +0530

    Create InstanceVariable.java

---
+++ 05-Types of Variables/InstanceVariable.java
@@ -0,0 +1,10 @@
+public class InstanceVariable{
+    String country="india";
+
+    public static void main(String[] args){
+        InstanceVariable instanceVariable= new InstanceVariable();
+        System.out.println(instanceVariable.country);
+
+    }
+
+}
