7ddb1584851ba7292da89633f6425791fcaf3a2f
jp-devhub <jpdevhub@gmail.com>
2026-02-07 14:34:27 +0530

    Create Variable.java

---
+++ 04-Variables/Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+
+   public static void main(String[] args){
+
+        int a = 25;
+
+        System.out.println(a);
+
+        int b = 100;
+
+        System.out.println(b);
+ 
+        int c =500;
+
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        
+   }
+}
